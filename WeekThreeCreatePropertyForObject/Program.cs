cdabd5d30badcc0e9d99507d70d6a8e5af29401b
Haven <haven.eldebaque@gmail.com>
2023-06-14 18:26:29 +1000

    Add project files.

---
+++ WeekThreeCreatePropertyForObject/Program.cs
@@ -0,0 +1,18 @@
+﻿// 3.6 Creating properties
+
+class Employee
+{
+    // create attributes
+    public int employeeNumber;
+    public string employeeName; 
+
+    static void Main()
+    {
+        Employee developer = new Employee();
+        developer.employeeNumber = 1;
+        developer.employeeName = "Haven Eldebaque";
+        Console.WriteLine("****** Employee Staff ******");
+        Console.WriteLine(developer.employeeNumber);
+        Console.WriteLine(developer.employeeName);
+    }
+}
